d9ff2c219d8be91d5e3fbcb88fdf2d3c0ee59dd5
Matthew Giem <matthew.giem@gmail.com>
2017-10-09 12:29:15 -0700

    basic working program

--- rowing_tracker.py
+++ rowing_tracker.py
@@ -1,3 +1,10 @@
+times = []
+class session:
+    def __init__(self, date, distance, time):
+        self.date = date
+        self.distance = distance
+        self.time = time
+
 def greeting():
     print("welcome to my program to track your rowing progress")
     print("if at any time you wish to quit enter 'quit'")
@@ -5,17 +12,31 @@ def greeting():
     print("to enter a new time enter 'new'")
     print("if you wish to see this again enter 'rules'")
 
+def history():
+    if len(times) > 0:
+        for history in times:
+            print("on {} your time for {} was {}".format(history.date, history.distance, history.time))
+    else:
+        print("there are no times saved yet")
+
+def new():
+    date = raw_input("date >>  ")
+    distance = raw_input("distance >>  ")
+    time = raw_input("time >>  ")
+    new_session = session(date, distance, time)
+    times.append(new_session)
+    history()
+
 while True:
     greeting()
-    prompt = raw_input(">> ").lower()
+    prompt = raw_input(">>  ").lower()
     if prompt == 'quit':
         break
     elif prompt == 'history':
-        pass
+        history()
     elif prompt == 'new':
-        pass
+        new()
     elif prompt == 'rules':
         greeting()
     else:
         print("your answer didn't compute")
-        # greeting()
